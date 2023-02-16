81c54cb03dd012dfb07a180c3395df7b339857bd
alphayangg <alphayohanna331@gmail.com>
2023-02-17 00:38:45 +0100

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,16 @@
-#include<stdii.h>
+#include <stdii.h>
 /**
- * main -A program that print a line with puts function
- * Return:0(success)
+ * main -prits  that print "programming is like building a multilingual puzzle,followed by a newline,
+ * Return:Always 0
  */
 int main(void)
+
 {
-puts("/""Programming is like building a multilingual puzzle");
-return(0);
+
+
+   puts("/"Programming is like building a multilingual    puzzle");
+   return (0);
+
+
 }
+
