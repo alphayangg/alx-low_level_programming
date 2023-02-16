9d3d649215dc934f847ad58625b3a8c8f37b871f
alphayangg <alphayohanna331@gmail.com>
2023-02-17 00:22:47 +0100

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include<stdio.h>
+#include<stdii.h>
 /**
  * main -A program that print a line with puts function
- * Return:0(when it return zero thiS whole thing is success)
- */ 
+ * Return:0(success)
+ */
 int main(void)
 {
-puts("/"Programming is like building a multilingual puzzle");
+puts("/""Programming is like building a multilingual puzzle");
 return(0);
 }
