a5e04974b468c2382adf4f0ad4febb21d8d0c216
alphayangg <alphayohanna331@gmail.com>
2023-02-19 22:49:37 +0100

    task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,12 +6,13 @@
  */
 int main(void)
 {
-        int c = 0;
+	int c = 0;
 
-        while (c < 10)
-        {
-		putchar(48 + c);
-                c++;
+	while (c < 10)
+	{
+	putchar(48 + c);
+	c++;
 	}
 	putchar('\n');
 	return (0);
+}
