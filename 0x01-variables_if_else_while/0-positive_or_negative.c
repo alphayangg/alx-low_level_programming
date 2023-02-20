57c7177c3e371b6a00ee757640ab083b91566662
alphayangg <alphayohanna331@gmail.com>
2023-02-20 01:18:27 +0100

    task0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,8 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main -print if number is greater than zero ispositive,and if is zero is
- zero and if number is less than zero is  negative
+ * main -print if number is positive , zero or negative
  * Return: Always(sucess)
 */
 int main (void)
@@ -20,7 +19,7 @@ int main (void)
 	{
 	printf ("%d is zero\n", n);
 	}
-	else if (n < 0)
+	else
 	{
 	printf ("%d is negatve\n", n);
 	}
