25697a9cc04527e84fa2ba4ccd0915020bdec41f
alphayangg <alphayohanna331@gmail.com>
2023-02-20 22:46:37 +0100

    task5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,13 +5,13 @@
  * main -Entry point
  * Return: Always 0(success)
  */
-int main(void)
+int main(void) 
 {
 
 int i;
 
-for (i = 0; i < 10; i++);
-printf("%d",i);
+for(i = 0; i < 10; i++);
+printf("%d", i);
 printf("\n");
 return (0);
 }
