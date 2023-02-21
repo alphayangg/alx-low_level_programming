bbfae1c169d466840ba5c8071b81f25397bc9d56
alphayangg <alphayohanna331@gmail.com>
2023-02-21 03:43:30 +0100

    task11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+#include <stdlib.h>                               /**                                                * main - main block
+ * Description: Write a program that prints all po
+ssible differnt
+ * Return: 0
+ */                                               int main(void)
+{
+        int c;                                            int d;                                            int e = 0;                                
+        while (e < 0)                                     {                                                         d = 0;                                            while (d < 10)
+                {                                                         c = 0;
+                        while (c < 10)
+                        {                                                         if (c != d && d !=
+ e && e < d && d < c)                                                             {                 @@@
