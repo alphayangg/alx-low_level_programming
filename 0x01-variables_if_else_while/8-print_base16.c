8034420fdb4d685eca36b7b97f53b284fd5d8707
alphayangg <alphayohanna331@gmail.com>
2023-02-21 03:30:06 +0100

    task8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: program that prints all the numbers of base 16
+ * Return: 0
+ */           int mani(void)                               
+{
+	 char c = 'z';
+	 while (c >= 'a')
+	 {
+         putchar(c);
+	 c--;
+	 }
+	 putchar('\n');
+	 return (0);
+}
