545c702a86bb5d4b1b24cc35f3eb2545229517fc
alphayangg <alphayohanna331@gmail.com>
2023-02-22 16:57:47 +0100

    task4

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,7 +2,7 @@ int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
-int _isalphabet(int c);                                    int print_sign(int n);
+int _isalpha(int c);                                    int print_sign(int n);
 int _abs(int n);
 int print_last_digit(int);                                 void jack_bauer(void);
 void time_table(void);
