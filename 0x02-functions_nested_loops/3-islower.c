f0e004f76af17942a085bbfbb7c2649e95654b1e
alphayangg <alphayohanna331@gmail.com>
2023-02-22 10:58:51 +0100

    task3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,9 +4,9 @@
  * @c: The character to be checked
  * Return: | for lowercase character or 0 for anything else
  */
-int _islower(int c)
+int _islower (int c)
 {
-	if(c >= 97 &&c <= 122)
+	if(c >= 97 && c <= 122)
 	{
 	return (1);
 	}
