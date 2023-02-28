4939138c796bce67a9a688e086dc9a82a5449ecc
alphayangg <alphayangg>
2023-02-28 23:11:59 +0100

    task2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -6,13 +6,9 @@
 * @c: third integer
 * Return: largest number
 */
-
 int largest_number(int a, int b, int c)
-
 {
-
 int largest;
-
 if (a > b && a > c)
 {
 largest = a;
@@ -29,6 +25,5 @@ else
 {
 largest = c;
 }
-	
 return (largest);
 }
