3719013fc98b8e1cde7f640eeeb755200e384462
alphayangg <alphayangg>
2023-02-28 21:52:33 +0100

    task8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,19 +1,23 @@
-
 #include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
 /**
- * main - main block
- * Description: program that prints all the numbers of base 16
- * Return: 0
- */           int mani(void)                               
+ * main - prints all the numbers of base 16 in lowercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-	 char c = 'z';
-	 while (c >= 'a')
-	 {
-         putchar(c);
-	 c--;
-	 }
-	 putchar('\n');
-	 return (0);
+	int n;
+
+	char ch;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
 }
