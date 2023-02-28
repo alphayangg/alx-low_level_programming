1d114c6a196c4965cf1fe5fced0443354dbf84f8
alphayangg <alphayangg>
2023-02-28 21:57:40 +0100

    task9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,22 +1,21 @@
-9
-
 #include <stdio.h>
-#include <stdlib.h>
 /**
- * main - main block
- * Description: program that prints all possible combinations
- * Return: 0
+ * main - prints all possible combinations of single-digit numbers
+ * Return: Always 0 (Success)
  */
-int main(void)                                    {                                                         int c = 0;
+int main(void)
+{
+	int n;
 
-        while (c < 10)                                    {                                                 putchar(48 + c);
-        if (c != 9)
-        {
-                putchar(',');
-                putchar(' ');
-        }
-        c++;
-        }
-        putchar('\n');
-        return (0);
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
