ea622f6413627e8eb970c136db912fae9db1e6eb
alphayangg <alphayangg>
2023-02-28 22:07:33 +0100

    taskt10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,20 +1,28 @@
-
 #include <stdio.h>
-#include <stdlib.h>
 /**
- * main - main block                               * Description: Write a program that prints all possible differe
- * Return: 0
+ * main - prints all possible different combinations of two digits
+ * Return: ALways 0 (Success)
  */
-int main(void)                                    {
-        int c;
-        int d = 0;
-                                                          while (d < 10)                                    {
-                c = 0;
-                while (c < 10)                                    {                                                         if (d != c && d < c)
-                        {
-                                putchar('0' + d);
-                                putchar('0' + c);
-                                if (c + d != 17)
-                                {
-                                        putchar(',');
-100-print_comb3.c               5,64           Top
+int main(void)
+{
+	int n, m;
+
+	for (n = 48; n <= 56; n++)
+	{
+		for (m = 49; m <= 57; m++)
+		{
+			if (m > n)
+			{
+				putchar(n);
+				putchar(m);
+				if (n != 56 || m != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
