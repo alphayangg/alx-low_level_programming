c8721c7b0e19ba71bf67ba371c77ad9508ba1cc7
alphayangg <alphayangg>
2023-03-02 13:43:10 +0100

    task11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -34,6 +34,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int overflow = 0, i = 0, j = 0, digits = 0;
 	int val1 = 0, val2 = 0, temp_tot = 0;
+
 	while (*(n1 + i) != '\0')
 		i++;
 	while (*(n2 + j) != '\0')
@@ -45,8 +46,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	while (j >= 0 || i >= 0 || overflow == 1)
 	{
 		if (i < 0)
-
-                        val1 = 0;
+			val1 = 0;
 		else
 			val1 = *(n1 + i) - '0';
 		if (j < 0)
