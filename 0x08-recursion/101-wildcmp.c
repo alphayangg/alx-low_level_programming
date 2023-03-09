6d513e0a86442c3b957ce3332023ac877a4578b8
alphayangg <alphayohanna331@gmail.com>
2023-03-09 14:20:17 -0800

    task8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -11,12 +11,11 @@ int wildcmp(char *s1, char *s2)
 	{
 		if (*s2 != '\0' && *s2 == '*')
 		{
-
-                        return (wildcmp(s1, s2 + 1));
-
-                }
+			return (wildcmp(s1, s2 + 1));
+		}
 		return (*s2 == '\0');
 	}
+
 	if (*s2 == '*')
 	{
 		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
@@ -24,8 +23,6 @@ int wildcmp(char *s1, char *s2)
 	else if (*s1 == *s2)
 	{
 		return (wildcmp(s1 + 1, s2 + 1));
-
-        }
-
-        return (0);
+	}
+	return (0);
 }
